1ce4aadd5032be92d0e40d98b67883a3574adae4
throwchris <69865554+throwchris@users.noreply.github.com>
2023-10-04 18:17:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,199 @@
+function toggleTextColor() {
+    var textElement = document.getElementById("text");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor2() {
+    var textElement = document.getElementById("text2");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor3() {
+    var textElement = document.getElementById("text3");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor4() {
+    var textElement = document.getElementById("text4");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor5() {
+    var textElement = document.getElementById("text5");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor6() {
+    var textElement = document.getElementById("text6");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor7() {
+    var textElement = document.getElementById("text7");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor8() {
+    var textElement = document.getElementById("text8");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor9() {
+    var textElement = document.getElementById("text9");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor10() {
+    var textElement = document.getElementById("text10");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor11() {
+    var textElement = document.getElementById("text11");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor12() {
+    var textElement = document.getElementById("text12");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor13() {
+    var textElement = document.getElementById("text13");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor14() {
+    var textElement = document.getElementById("text14");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor15() {
+    var textElement = document.getElementById("text15");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor16() {
+    var textElement = document.getElementById("text16");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor17() {
+    var textElement = document.getElementById("text17");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor18() {
+    var textElement = document.getElementById("text18");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor19() {
+    var textElement = document.getElementById("text19");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
+
+function toggleTextColor20() {
+    var textElement = document.getElementById("text20");
+
+    if (textElement.classList.contains("w3-text-light-grey")) {
+        textElement.classList.remove("w3-text-light-grey");
+    } else {
+        textElement.classList.add("w3-text-light-grey");
+    }
+}
